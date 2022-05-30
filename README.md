8d16f459972d3d79afc2fe2d61db137a0bab22a7
Yuniel <yreyes8827@gmail.com>
2022-05-30 18:33:37 +0200

    Add button to change grid size Refactor to add event handlers every time the grid gets updated

--- README.md
+++ README.md
@@ -22,18 +22,37 @@
 >   * For each iteration of the inner lop, create a div element and update
       its height to the corresponding according to the formula below.
 >   * The function should create a 16x16 grid of square divs
->   * Get the total number of squares = 16 * 16 = 256 squares  
->   * Get area of each square:
->        - Divide the total area of the big square: 552*552 = 304704
->          between the total number of squares: 304704 / 256.
-           That will give me the are of each square: 1190.25
->   * Get height of each square: sqrt 1190  = 34
+>   * Create a function called `gridsHeight()`
+>       
+    - Get the total number of squares = x * x = x² squares  
+    - Get area of each square:
+        - Divide the total area of the big square: 552*552 = 304704
+          between the total number of squares: 304704 / x².
+        - That will give me the area of each square: 
+      Get height of each square: sqrt of (304704 / x²)  
 >   * Width will be guarantied by the flex-grow property.  
 
-2. Set up a 'hover' effect so that the each div-grid change color
+2. Set up a 'hover' effect so that each div-grid change color
 when the mouse passes over them, leaving a (pixelated) trail through
 your grid like a pen would.
 >   * Get a reference of every div-grid
 >   * Set up an event listener that change the background color of
-    the div-grid you're hovering over
->   * Let's use for now the toggle() method of the classList property    
+    the div-grid you're hovering over.
+
+    Bottleneck: 
+        - You have to choose between an HTMLCollection or a NodeList, as
+        every time the big square is updated, new grid-divs are added and
+        you need to keep track of all of them. 
+        Solution: Create an event handler for every time a new square is
+        generated (inside the makeGrid() function)
+
+>   * Let's use for now the toggle() method of the classList property
+3. Create a load event that call the `makeGrid()` function at leat onces
+   every time the page is loaded.  
+4. Add a button to the top of the screen. 
+>   * The button should pop up a box asking the user for the number of
+      square per side for the grid. Once entered, the existing grid 
+      should be removed and a new grid should be generated in the same
+      total space as before. 
+      Tip: Set limit ot a maximum of 100 squares. 
+         
--- myScript.js
+++ myScript.js
@@ -1,32 +1,77 @@
-// Get grid node reference
-const theGrid = document.getElementById('main-container');
+// Create function gridsHeight
+function gridsHeight(gridNumber){
+    const BIG_SQUARE_AREA = Math.pow(650, 2);
+    const squareTotal = Math.pow(gridNumber, 2);
+    const THIS_HEIGHT = Math.sqrt(BIG_SQUARE_AREA / squareTotal);
+    return THIS_HEIGHT;
+}
 
-
-// Make a 16x16 grid of square divs
+// Make a YxY grid of square divs
 // Create two loops:  
-for (let outer = 0; outer < 16; outer++){  // For each iteration of the outer loop, the inner loop will iterate 16 times
-    for (let inner = 0; inner < 16; inner++){ 
-        let newDiv = document.createElement('div'); // For each iteration of the inner lop, create a div element
-        newDiv.classList.add('eachDiv'); // Add to it the eachDiv class
-        theGrid.appendChild(newDiv);
+function makeGrid(gridNumber = 1){
+    const THIS_HEIGHT = gridsHeight(gridNumber); // Get height of each div-grid according to number of grids
+    for (let outer = 0; outer < Number(gridNumber); outer++){  // For each iteration of the outer loop, the inner loop will iterate 16 times
+        for (let inner = 0; inner < Number(gridNumber); inner++){ 
+            let newDiv = document.createElement('div'); // For each iteration of the inner lop, create a div element
+            newDiv.classList.add('eachDiv'); // Add to it the eachDiv class
+            newDiv.style.height = THIS_HEIGHT.toString() + "px"; // Convert height to string and pass it with its units
+            theGrid.appendChild(newDiv);
+        }
+        if (gridNumber > 1){
+            // For each iteration of the outer loop, a break div has to be inserted:
+            // Create breakDiv element
+            let breakDiv = document.createElement('div');
+            breakDiv.classList.add('break');
+            theGrid.appendChild(breakDiv);    
+        }
     }
-    // For each iteration of the outer loop, a break div has to be inserted:
-    // Create breakDiv element
-    let breakDiv = document.createElement('div');
-    breakDiv.classList.add('break');
-    theGrid.appendChild(breakDiv);    
+    allDivGrid = theGrid.querySelectorAll('.eachDiv');
+    allDivGrid.forEach(div => div.addEventListener('mouseover', changeColor));
+    return;
 }
 
 
 // Create changeColor() function
 function changeColor(e){
     e.stopPropagation();
-    e.target.classList.toggle("colorChange"); // active the colorChange class 
+    e.target.classList.toggle("colorChange"); // activate/deactivate the colorChange class 
 }
 
+// getNumber ask the user for a number for the grid
+// and remove all elements of the main-container
+function getNumber(){
+    let thisNumber = prompt("Grid number");
+    while (thisNumber > 100){
+        alert("Grid number can't be greater than 100");
+    }
+    while (theGrid.hasChildNodes()){ // remove every grid-div from the old big grid before 
+        theGrid.removeChild(theGrid.firstChild) // generating the new one
+    }
+    makeGrid(thisNumber);
+}
+
+
+// Call the function makeGrid at least once every time the page is load
+window.onload = function(){
+    makeGrid();
+};
+
+
+// ==REFERENCES==
+// Get grid node reference
+const theGrid = document.getElementById('main-container');
+
+
 // Get reference of each div-grid
-const allDivGrid = document.querySelectorAll('.eachDiv');
+// Use a live nodeList instead of a static one
+// as grids will be constantly updated  
+let allDivGrid = theGrid.querySelectorAll('.eachDiv');
 // For every grid-div with the pointer over it, 
 // call the function changeColor()
 allDivGrid.forEach(div => div.addEventListener('mouseover', changeColor));
 
+
+// Get button reference
+const theButton = document.querySelector('.big-button');
+// Add event listener for when the button is clicked
+theButton.addEventListener('click', getNumber);
